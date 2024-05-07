2dd95de99e1d3ce3b5a572ecd50b974786f28544
Fahri <117529823+Sidhsksjsjsh@users.noreply.github.com>
2024-05-07 22:59:07 +0900

    Create LocalScript.lua

---
+++ LocalScript.lua
@@ -0,0 +1,4 @@
+--https://raw.githubusercontent.com/Sidhsksjsjsh/Emailsender/main/Module.lua
+local bot = loadstring(game:HttpGet("https://raw.githubusercontent.com/Sidhsksjsjsh/Emailsender/main/Module.lua"))()
+local Output = bot:SendEmail("kinkinlinda29@gmail.com","Test","Hello this is a test message from Roblox API & Google Script")
+print(Output)
